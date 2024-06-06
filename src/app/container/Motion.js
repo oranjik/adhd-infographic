cf57dd1d688b4d1aa180382807e9c04d7853973c
oranjik <jieun.kim0413@gmail.com>
2024-06-06 18:56:24 +0900

    Change images and change language

--- src/app/container/Motion.js
+++ src/app/container/Motion.js
@@ -2,13 +2,17 @@ import contents from "./contents.module.css";
 import motion from "./motion.module.css";
 import circle from "./circle.module.css";
 import { Video } from "./Video";
+import { useTranslation } from "../contexts/TranslationContext";
 export default function Motion({ src }) {
+  const { switchLanguage, language } = useTranslation();
   return (
     <div className={motion.container}>
       <div className={circle.circlePadding} />
       <Video src={src} className={motion.motionBig} />
       <div className={motion.description}>
-        Tap left or right arrow to see each feeling
+        {language == "ko"
+          ? "왼쪽과 오른쪽 화살표를 클릭하여 감정을 확인해보세요!"
+          : "Tap left or right arrow to see each feeling!"}
       </div>
     </div>
   );
